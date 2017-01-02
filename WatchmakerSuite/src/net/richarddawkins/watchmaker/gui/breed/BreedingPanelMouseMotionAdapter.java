5a8f135e621bec59e139db4e47a94c453479ceb7
Alan Canon <aronnax9000@gmail.com>
2017-01-02 11:45:34 -0500

    Add files via upload

---
+++ WatchmakerSuite/src/net/richarddawkins/watchmaker/gui/breed/BreedingPanelMouseMotionAdapter.java
@@ -0,0 +1,40 @@
+package net.richarddawkins.watchmaker.gui.breed;
+
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import net.richarddawkins.watchmaker.gui.genebox.GeneBoxStrip;
+import net.richarddawkins.watchmaker.morph.common.Morph;
+
+public class BreedingPanelMouseMotionAdapter implements MouseMotionListener {
+
+	private BreedingPanel panel;
+	public BreedingPanelMouseMotionAdapter(BreedingPanel panel) {
+		this.panel = panel;
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+		Point myPt = e.getPoint();
+		switch(panel.phase) {
+		case breed_complete:
+			int boxNo = panel.boxesDrawer.getBoxNoContainingPoint(myPt, panel.getSize());
+			  if(boxNo != -1) {
+				  Morph morph = panel.getMorphs().elementAt(boxNo);
+				  GeneBoxStrip geneBoxStrip = panel.getMorphConfig().getGeneBoxStrip();
+				  geneBoxStrip.setGenome(morph.getGenome());
+			  }
+			  break;
+	    default:
+		}
+		
+	}
+}
