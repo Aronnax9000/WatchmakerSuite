b2bab61918e8b5e59353130e7a4b272e3167bdc9
Alan Canon <aronnax9000@gmail.com>
2017-01-02 17:16:42 -0500

    WatchmakerJ

--- WatchmakerJ/src/net/richarddawkins/watchmaker/BreedingPanel.java
+++ WatchmakerJ/src/net/richarddawkins/watchmaker/BreedingPanel.java
@@ -1,8 +1,6 @@
 package net.richarddawkins.watchmaker;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
