b98030e2ce8340da94540f6ada98fbc4a55909cb
Alan Canon <aronnax9000@gmail.com>
2017-01-20 10:08:41 -0500

    Add src folder to WatchmakerApp

---
+++ WatchmakerApp/src/net/richarddawkins/watchmaker/app/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Alan Canon
+ * This package, and its subpackages, represent 
+ * GUI-platform independent abstract classes
+ * for the chief application components of Watchmaker.
+ * 
+ */
+package net.richarddawkins.watchmaker.app;
