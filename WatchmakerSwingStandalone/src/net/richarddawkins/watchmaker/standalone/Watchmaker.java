bda5baaaa2312c2df2b1ff352ecaeea943573f27
Alan Canon <aronnax9000@gmail.com>
2017-01-28 19:38:27 -0500

    Standalone

---
+++ WatchmakerSwingStandalone/src/net/richarddawkins/watchmaker/standalone/Watchmaker.java
@@ -0,0 +1,12 @@
+package net.richarddawkins.watchmaker.standalone;
+
+import net.richarddawkins.watchmaker.swing.Main;
+
+public class Watchmaker {
+
+	public static void main(String[] args) {
+		Main.main(args);
+
+	}
+
+}
